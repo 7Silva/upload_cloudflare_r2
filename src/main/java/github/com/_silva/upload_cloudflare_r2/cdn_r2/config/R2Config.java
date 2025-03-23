8171dd516b1e23fe06b061af03b05d88d196a8b6
7Silva <zaronymogamer@gmail.com>
2025-03-23 00:22:49 -0300

    ✨ feat: Configure S3Client for Cloudflare R2 compatibility
    - Add S3Configuration to enable path-style access
    - Disable checksum validation
    - Set region to "auto" for R2 compatibility

--- src/main/java/github/com/_silva/upload_cloudflare_r2/cdn_r2/config/R2Config.java
+++ src/main/java/github/com/_silva/upload_cloudflare_r2/cdn_r2/config/R2Config.java
@@ -10,6 +10,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 @Configuration
 public class R2Config {
@@ -25,11 +26,16 @@ public class R2Config {
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        S3Configuration s3Config = S3Configuration.builder()
+                .pathStyleAccessEnabled(true)
+                .checksumValidationEnabled(false)
+                .build();
 
         return S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .endpointOverride(URI.create(endpoint))
-                .region(Region.US_EAST_1)
+                .serviceConfiguration(s3Config)
+                .region(Region.of("auto"))
                 .build();
     }
 }
